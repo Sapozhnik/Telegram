5e28cb77a93f9bccd126377fe2e038de16c5dde2
X140Yu <zhaoxinyu1994@gmail.com>
2017-04-05 22:30:14 +0800

    Make Telegram for iOS compile

---
+++ config.h
@@ -0,0 +1,15 @@
+//
+//  config.h
+//  Telegraph
+//
+//  Created by X140Yu on 5/4/2017.
+//
+//
+
+#ifndef config_h
+#define config_h
+
+#define SETUP_API_ID(v) v = 2834;
+#define SETUP_API_HASH(v) v = @"68875f756c9b437a8b916ca3de215815";
+
+#endif /* config_h */
